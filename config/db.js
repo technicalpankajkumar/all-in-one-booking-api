79d3a30fe6c1fcda40655b9458bf3fd795e4f4fc
technicalpankajkumar <technicalpankajkumar@gmail.com>
2025-03-02 22:04:57 +0530

    development complete branch

--- config/db.js
+++ config/db.js
@@ -1,3 +1,15 @@
 
 import { PrismaClient } from '@prisma/client';
 export const db = new PrismaClient();
+
+export const connectToDatabase = async () =>{
+    try {
+        await db.$connect();
+        console.log('Database connected successfully')
+    } catch (error) {
+        console.log('error connection to database : ', error.message)
+    } finally{
+        await db.$disconnect();
+    }
+}
+
