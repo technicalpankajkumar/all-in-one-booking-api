ed1b2cf51fdb28233eb1c394fe76ee07edc1cfcf
pankajkumar <technicalpankajkumar@gmail.com>
2025-12-02 11:21:12 +0530

    setup prisma

--- config/db.js
+++ config/db.js
@@ -1,15 +1,15 @@
+import pkg from "@prisma/client";
+const { PrismaClient } = pkg;
 
-import { PrismaClient } from '@prisma/client';
 export const db = new PrismaClient();
 
-export const connectToDatabase = async () =>{
-    try {
-        await db.$connect();
-        console.log('Database connected successfully')
-    } catch (error) {
-        console.log('error connection to database : ', error.message)
-    } finally{
+export const connectToDatabase = async () => {
+  try {
+    await db.$connect();
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.error("Error connecting to the database:", error.message);
+  }finally{
         await db.$disconnect();
     }
-}
-
+};
